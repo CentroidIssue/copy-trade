790b7030c002a22b89003ea0992cb426853cf2c9
jaibtdou <iamnoob2210@gmail.com>
2023-12-15 23:04:37 +0700

    Updated for multi portfolio IDS

--- copy-trade.js
+++ copy-trade.js
@@ -5,25 +5,38 @@ const public = require('./config/public.json');
 const noti = require('./noti.js')
 const fs = require('fs');
 const { exit } = require('process');
+const { table } = require('console');
 const db = new sqlite3.Database('database/binance.db');
-const url = `https://www.binance.com/bapi/futures/v1/friendly/future/copy-trade/lead-data/positions?portfolioId=${public.PROFILE_ID}`;
+const base_url = "https://www.binance.com/bapi/futures/v1/friendly/future/copy-trade/lead-data/positions?";
 
 console.log("Opened database successfully");
 
 async function init_sqlite3() {
-    const response = await axios.get(url);
-    const data = response.data.data;
-
+    //get all data for all public.PROFILE_ID using axios asynchronously
+    const promises = public.PROFILE_ID.map(async (id) => {
+        const url = base_url + "portfolioId=" + id.ID;
+        const response = await axios.get(url);
+        return response;
+    });
+    //wait for all promises to be resolved
+    const responses = await Promise.all(promises);
+    //store all responses in response.data
+    responses.data = responses.map((response) => response.data);
+    //write response to database
     db.serialize(() => {
-        db.run(`CREATE TABLE IF NOT EXISTS positions
+        //table name is public.PROFILE_ID[].ID
+        public.PROFILE_ID.forEach((id) => {
+            db.run(`CREATE TABLE IF NOT EXISTS '${id.ID}'
                 (id text, symbol text, collateral text, positionAmount text, entryPrice text, unrealizedProfit text, cumRealized text, askNotional text, bidNotional text, notionalValue text, markPrice text, leverage text, isolated text, isolatedWallet text, adl text, positionSide text, breakEvenPrice text)`);
-
-        db.run("DELETE FROM positions");
-
-        data.forEach(position => {
-            db.run(`INSERT INTO positions VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, [position['id'], position['symbol'], position['collateral'], position['positionAmount'], position['entryPrice'], position['unrealizedProfit'], position['cumRealized'], position['askNotional'], position['bidNotional'], position['notionalValue'], position['markPrice'], position['leverage'], position['isolated'], position['isolatedWallet'], position['adl'], position['positionSide'], position['breakEvenPrice']]);
+            //Delete all data in table
+            db.run(`DELETE FROM '${id.ID}'`);
+            //Insert data into table
+            responses.data[public.PROFILE_ID.indexOf(id)].data.forEach((position) => {
+                db.run(`INSERT INTO '${id.ID}' VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, [position['id'], position['symbol'], position['collateral'], position['positionAmount'], position['entryPrice'], position['unrealizedProfit'], position['cumRealized'], position['askNotional'], position['bidNotional'], position['notionalValue'], position['markPrice'], position['leverage'], position['isolated'], position['isolatedWallet'], position['adl'], position['positionSide'], position['breakEvenPrice']]);
+            });
         });
     });
+
 }
 
 /**
@@ -41,83 +54,79 @@ function serialize_message(message) {
 async function run() {
     try {
         while (true) {
-            const response = await axios.get(url);
-            //write response to copy-trade.json with identation
-            fs.writeFile('copy-trade.json', JSON.stringify(response.data, null, 2), function (err) {
-                if (err) return console.log(err);
+            //get all data for all public.PROFILE_ID using axios asynchronously
+            const promises = public.PROFILE_ID.map(async (id) => {
+                const url = base_url + "portfolioId=" + id.ID;
+                const response = await axios.get(url);
+                return response;
             });
-            const data = response.data.data;
-            data.forEach(async position => {
-                let decimal = 0;
-                if (position['positionAmount'].includes('.')) {
-                    decimal = position['positionAmount'].split('.')[1].length;
-                }
-                let quantity = parseFloat(position['positionAmount'], 10);
-                const side = position['positionSide'];
-
-                db.get("SELECT * FROM positions WHERE id=?", [position['id']], (err, row) => {
-                    if (err) {
-                        return console.error(err.message);
+            //wait for all promises to be resolved
+            const responses = await Promise.all(promises);
+            //store all responses in response.data
+            responses.data = responses.map((response) => response.data);
+            //For all public.PROFILE_ID adn write data to ID.json
+            public.PROFILE_ID.forEach((id) => {
+                fs.writeFile('database/' + id.ID + '.json', JSON.stringify(responses.data[public.PROFILE_ID.indexOf(id)], null, 2), function (err) {
+                    if (err) return console.log(err);
+                });
+            });
+            //For all public.PROFILE_ID and compare data with database
+            public.PROFILE_ID.forEach((id) => {
+                const data = responses.data[public.PROFILE_ID.indexOf(id)].data;
+                data.forEach(async position => {
+                    let decimal = 0;
+                    if (position['positionAmount'].includes('.')) {
+                        decimal = position['positionAmount'].split('.')[1].length;
                     }
-                    if (!row) {
-                        db.run(`INSERT INTO positions VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, [position['id'], position['symbol'], position['collateral'], position['positionAmount'], position['entryPrice'], position['unrealizedProfit'], position['cumRealized'], position['askNotional'], position['bidNotional'], position['notionalValue'], position['markPrice'], position['leverage'], position['isolated'], position['isolatedWallet'], position['adl'], position['positionSide'], position['breakEvenPrice']]);
-                        console.log(public.USER_ID[0], "New position:\n" + serialize_message(position));
-                        if (side == "LONG") {
-                            quantity = quantity * public.ORDER_SCALE;
-                            //Round to decimal
-                            quantity = quantity.toFixed(decimal);
-                            futures.futures_long_buying(position['symbol'], quantity);
+                    let quantity = parseFloat(position['positionAmount'], 10);
+                    const side = position['positionSide'];
+                    db.get(`SELECT * FROM '${id.ID}' WHERE id=?`, [position['id']], (err, row) => {
+                        if (err) {
+                            return console.error(err.message);
                         }
-                        else if (side == "SHORT"){
-                            quantity = -quantity;
-                            quantity = quantity * public.ORDER_SCALE;
-                            //Round to decimal
-                            quantity = quantity.toFixed(decimal);
-                            futures.futures_short_selling(position['symbol'], quantity);
-                        }
-                        else if (side == "BOTH"){
-                            if (quantity > 0) {
-                                quantity = quantity * public.ORDER_SCALE;
+                        if (!row) {
+                            db.run(`INSERT INTO '${id.ID}' VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, [position['id'], position['symbol'], position['collateral'], position['positionAmount'], position['entryPrice'], position['unrealizedProfit'], position['cumRealized'], position['askNotional'], position['bidNotional'], position['notionalValue'], position['markPrice'], position['leverage'], position['isolated'], position['isolatedWallet'], position['adl'], position['positionSide'], position['breakEvenPrice']]);
+                            console.log(id.ID, "New position:\n" + serialize_message(position));
+                            if (side == "LONG") {
+                                quantity = quantity * id.ORDER_SCALE.NUME / id.ORDER_SCALE.DENO;
                                 //Round to decimal
                                 quantity = quantity.toFixed(decimal);
                                 futures.futures_long_buying(position['symbol'], quantity);
                             }
-                            else if (quantity < 0){
+                            else if (side == "SHORT"){
                                 quantity = -quantity;
-                                quantity = quantity * public.ORDER_SCALE;
+                                quantity = quantity * id.ORDER_SCALE.NUME / id.ORDER_SCALE.DENO;
                                 //Round to decimal
                                 quantity = quantity.toFixed(decimal);
                                 futures.futures_short_selling(position['symbol'], quantity);
                             }
+                            else if (side == "BOTH"){
+                                if (quantity > 0) {
+                                    quantity = quantity * id.ORDER_SCALE.NUME / id.ORDER_SCALE.DENO;
+                                    //Round to decimal
+                                    quantity = quantity.toFixed(decimal);
+                                    futures.futures_long_buying(position['symbol'], quantity);
+                                }
+                                else if (quantity < 0){
+                                    quantity = -quantity;
+                                    quantity = quantity * id.ORDER_SCALE.NUME / id.ORDER_SCALE.DENO;
+                                    //Round to decimal
+                                    quantity = quantity.toFixed(decimal);
+                                    futures.futures_short_selling(position['symbol'], quantity);
+                                }
+                            }
+                            noti.messengerBotSendText(public.USER_ID[0], "New order: \n" + serialize_message(position));
                         }
-                        noti.messengerBotSendText(public.USER_ID[0], "New order: \n" + serialize_message(position));
-                    } else if (row.positionAmount != position['positionAmount']) {
-                        db.run("UPDATE positions SET positionAmount=? WHERE id=?", [position['positionAmount'], position['id']]);
-                        console.log(quantity, row.positionAmount, decimal);
-                        last_quantity = quantity;
-                        quantity = quantity - parseFloat(row.positionAmount, 10);
-                        console.log(quantity)
-                        quantity = quantity * public.ORDER_SCALE
-                        //Round to decimal
-                        quantity = quantity.toFixed(decimal);
-                        if (side == "LONG") {
-                            if (quantity > 0) {
-                                futures.futures_long_buying(position['symbol'], quantity);
-                            }
-                            else if (quantity < 0){
-                                futures.futures_long_selling(position['symbol'], -quantity);
-                            }
-                        }
-                        else if (side == "SHORT"){
-                            if (quantity < 0) {
-                                futures.futures_short_selling(position['symbol'], -quantity);
-                            }
-                            else if (quantity > 0){
-                                futures.futures_short_buying(position['symbol'], quantity);
-                            }
-                        }
-                        else if (side == "BOTH"){
-                            if (row.positionAmount > 0) {
+                        else if (row.positionAmount != position['positionAmount']) {
+                            db.run(`UPDATE '${id.ID}' SET positionAmount=? WHERE id=?`, [position['positionAmount'], position['id']]);
+                            console.log(quantity, row.positionAmount, decimal);
+                            last_quantity = quantity;
+                            quantity = quantity - parseFloat(row.positionAmount, 10);
+                            console.log(quantity)
+                            quantity = quantity * id.ORDER_SCALE.NUME / id.ORDER_SCALE.DENO
+                            //Round to decimal
+                            quantity = quantity.toFixed(decimal);
+                            if (side == "LONG") {
                                 if (quantity > 0) {
                                     futures.futures_long_buying(position['symbol'], quantity);
                                 }
@@ -125,7 +134,7 @@ async function run() {
                                     futures.futures_long_selling(position['symbol'], -quantity);
                                 }
                             }
-                            else if (row.positionAmount < 0){
+                            else if (side == "SHORT"){
                                 if (quantity < 0) {
                                     futures.futures_short_selling(position['symbol'], -quantity);
                                 }
@@ -133,30 +142,48 @@ async function run() {
                                     futures.futures_short_buying(position['symbol'], quantity);
                                 }
                             }
-                            else{
-                                if (quantity > 0) {
-                                    futures.futures_long_buying(position['symbol'], quantity);
+                            else if (side == "BOTH"){
+                                if (row.positionAmount > 0) {
+                                    if (quantity > 0) {
+                                        futures.futures_long_buying(position['symbol'], quantity);
+                                    }
+                                    else if (quantity < 0){
+                                        futures.futures_long_selling(position['symbol'], -quantity);
+                                    }
                                 }
-                                else if (quantity < 0){
-                                    futures.futures_short_selling(position['symbol'], -quantity);
+                                else if (row.positionAmount < 0){
+                                    if (quantity < 0) {
+                                        futures.futures_short_selling(position['symbol'], -quantity);
+                                    }
+                                    else if (quantity > 0){
+                                        futures.futures_short_buying(position['symbol'], quantity);
+                                    }
+                                }
+                                else{
+                                    if (quantity > 0) {
+                                        futures.futures_long_buying(position['symbol'], quantity);
+                                    }
+                                    else if (quantity < 0){
+                                        futures.futures_short_selling(position['symbol'], -quantity);
+                                    }
+                                }
+                                if (Math.abs(row.positionAmount) < Math.abs(last_quantity) && row.positionAmount * last_quantity < 0) {
+                                    quantity = last_quantity + row.positionAmount;
+                                    quantity = quantity * id.ORDER_SCALE.NUME / id.ORDER_SCALE.DENO;
+                                    //Round to decimal
+                                    quantity = quantity.toFixed(decimal);
+                                    if (quantity > 0) {
+                                        futures.futures_long_buying(position['symbol'], quantity);
+                                    }
+                                    else if (quantity < 0){
+                                        futures.futures_short_selling(position['symbol'], -quantity);
+                                    }
                                 }
                             }
-                            if (Math.abs(row.positionAmount) < Math.abs(last_quantity) && row.positionAmount * last_quantity < 0) {
-                                quantity = last_quantity + row.positionAmount;
-                                quantity = quantity * public.ORDER_SCALE;
-                                //Round to decimal
-                                quantity = quantity.toFixed(decimal);
-                                if (quantity > 0) {
-                                    futures.futures_long_buying(position['symbol'], quantity);
-                                }
-                                else if (quantity < 0){
-                                    futures.futures_short_selling(position['symbol'], -quantity);
-                                }
-                            }
+                            noti.messengerBotSendText(public.USER_ID[0], "Changed position: " + serialize_message(position) + "Initial position: " + row.positionAmount);
+                            console.log("Position amount of " + position['symbol'] + " has changed");
                         }
-                        noti.messengerBotSendText(public.USER_ID[0], "Changed position: " + serialize_message(position) + "Initial position: " + row.positionAmount);
-                        console.log("Position amount of " + position['symbol'] + " has changed");
-                    }
+                    });
                 });
             });
         }
